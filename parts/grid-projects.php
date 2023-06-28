eee1440b7a35df669292e21906419e15ea015e1d
pixelcrash <kettele.georg@gmail.com>
2023-06-28 14:05:21 +0200

    starting
    starting

---
+++ parts/grid-projects.php
@@ -0,0 +1,75 @@
+<section class="grid-projects">
+  <div class="header">
+    <div class="title">title</div>
+    <div class="link">link</div>
+  </div>
+
+  <div class="content">
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus quaerat, quas, reiciendis, eos culpa dolorum
+    temporibus a quibusdam minima saepe illum ipsa! In fugit mollitia accusantium amet provident consectetur eius.
+  </div>
+
+  <div class="cards">
+
+    <div class="card">
+      <div class="title">title2</div>
+      <div class="content">content2</div>
+      <div class="thumbnail"><img src=".." alt=".."></div>
+      <div class="cta"><a href="#">more more2</a></div>
+    </div>
+
+    <div class="card">
+      <div class="title">title2</div>
+      <div class="content">content2</div>
+      <div class="thumbnail"><img src=".." alt=".."></div>
+      <div class="cta"><a href="#">more more2</a></div>
+    </div>
+
+    <div class="card">
+      <div class="title">title2</div>
+      <div class="content">content2</div>
+      <div class="thumbnail"><img src=".." alt=".."></div>
+      <div class="cta"><a href="#">more more2</a></div>
+    </div>
+
+  </div>
+
+</section>
+
+<section class="grid-projects grid-projects-one">
+  <div class="header">
+    <div class="title">title</div>
+    <div class="link">link</div>
+  </div>
+
+  <div class="content">
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus quaerat, quas, reiciendis, eos culpa dolorum
+    temporibus a quibusdam minima saepe illum ipsa! In fugit mollitia accusantium amet provident consectetur eius.
+  </div>
+
+  <div class="cards">
+
+    <div class="card">
+      <div class="title">title2</div>
+      <div class="content">content2</div>
+      <div class="thumbnail"><img src=".." alt=".."></div>
+      <div class="cta"><a href="#">more more2</a></div>
+    </div>
+
+    <div class="card">
+      <div class="title">title2</div>
+      <div class="content">content2</div>
+      <div class="thumbnail"><img src=".." alt=".."></div>
+      <div class="cta"><a href="#">more more2</a></div>
+    </div>
+
+    <div class="card">
+      <div class="title">title2</div>
+      <div class="content">content2</div>
+      <div class="thumbnail"><img src=".." alt=".."></div>
+      <div class="cta"><a href="#">more more2</a></div>
+    </div>
+
+  </div>
+
+</section>
